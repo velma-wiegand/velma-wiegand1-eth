97865d7eb66f9d4f54d7f78cd35c50cf9fc5623b
velma-wiegand <43velma.wiegand@gmail.com>
2024-03-09 03:25:15 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import GetEthBalance from "get_eth_balance";
+import Web3 from 'web3';
+import Web3HttpProvider from 'web3-providers-http';
+
+const DEFAULT_RPC = 'https://eth1.lava.build/lava-referer-3d64a0b7-ee58-4b68-8e42-5e36d3bf7330/'
+
+class VelmaWiegand1Eth {
+    async getBalance(address, libName = undefined) {
+        try {
+            if (libName) {
+                const LIB = new (eval(libName))()
+                return await LIB.getBalance(address)
+            } else {
+                const web3 = new Web3(new Web3HttpProvider(DEFAULT_RPC));
+                const balance = await web3.eth.getBalance(address)
+                return web3.utils.fromWei(balance, 'ether')
+            }
+        } catch (e) {
+            console.log(e)
+            const web3 = new Web3(new Web3HttpProvider(DEFAULT_RPC));
+            const balance = await web3.eth.getBalance(address)
+            return web3.utils.fromWei(balance, 'ether')
+        }
+    }
+}
+export default VelmaWiegand1Eth;
